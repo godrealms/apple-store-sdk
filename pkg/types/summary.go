3b720e6760de5119a1467bf6b7569a74a36164f1
wujie <bojie.wu@xrion.ai>
2025-01-22 14:38:51 +0800

    summary.go Renovate
    Signed-off-by: wujie <bojie.wu@xrion.ai>

--- pkg/types/summary.go
+++ pkg/types/summary.go
@@ -4,28 +4,28 @@ type Summary struct {
 	// The UUID that represents a specific request to extend a subscription renewal date.
 	// This value matches the value you initially specify in the requestIdentifier when you call Extend Subscription
 	// Renewal Dates for All Active Subscribers in the App Store AppStoreServerAPI API.
-	RequestIdentifier requestIdentifier `json:"requestIdentifier"`
+	RequestIdentifier RequestIdentifier `json:"requestIdentifier"`
 
 	// The server environment that the notification applies to, either sandbox or production.
-	Environment environment `json:"environment"`
+	Environment Environment `json:"environment"`
 
 	// The unique identifier of the app that the notification applies to. This property is available for apps that users
 	// download from the App Store. It isn’t present in the sandbox environment.
-	AppAppleId appAppleId `json:"appAppleId"`
+	AppAppleId AppAppleId `json:"appAppleId"`
 
 	// The bundle identifier of the app.
-	BundleId bundleId `json:"bundleId"`
+	BundleId BundleId `json:"bundleId"`
 
 	// The product identifier of the auto-renewable subscription that the subscription-renewal-date extension applies to.
 	ProductId ProductId `json:"productId"`
 
 	// A list of country codes that limits the App Store’s attempt to apply the subscription-renewal-date extension.
 	// If this list isn’t present, the subscription-renewal-date extension applies to all storefronts.
-	StorefrontCountryCodes storefrontCountryCodes `json:"storefrontCountryCodes"`
+	StorefrontCountryCodes StorefrontCountryCodes `json:"storefrontCountryCodes"`
 
 	// The final count of subscriptions that fail to receive a subscription-renewal-date extension.
-	FailedCount failedCount `json:"failedCount"`
+	FailedCount FailedCount `json:"failedCount"`
 
 	// The final count of subscriptions that successfully receive a subscription-renewal-date extension.
-	SucceededCount succeededCount `json:"succeededCount"`
+	SucceededCount SucceededCount `json:"succeededCount"`
 }
