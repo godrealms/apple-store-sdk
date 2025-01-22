9536af56e1bd84fd405639aaebcc97666beef515
wujie <bojie.wu@xrion.ai>
2025-01-22 17:41:05 +0800

    Signature verification issue fixed
    Signed-off-by: wujie <bojie.wu@xrion.ai>

--- examples/AppStoreServerAPI/handle_notification.go
+++ examples/AppStoreServerAPI/handle_notification.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/godrealms/apple-store-sdk/pkg/services/AppStoreServerNotifications/ServerNotificationsVersion2"
+	"log"
+)
 
+func main() {
+	signedPayload := ""
+	notifications, err := serverNotificationsVersion2.Notifications(signedPayload)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	log.Printf("%+v\n", notifications)
 }
--- pkg/services/AppStoreServerNotifications/ResponseTypes/signedPayload.go
+++ pkg/services/AppStoreServerNotifications/ResponseTypes/signedPayload.go
@@ -2,12 +2,14 @@ package responseTypes
 
 import (
 	"crypto"
+	"crypto/ecdsa"
 	"crypto/rsa"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/godrealms/apple-store-sdk/pkg/types"
+	"math/big"
 	"strings"
 )
 
@@ -39,9 +41,23 @@ func (sp SignedPayload) DecodedPayload() (*ResponseBodyV2DecodedPayload, error) 
 	hash := sha256.Sum256([]byte(signedContent))
 
 	// Verify the signature
-	err = rsa.VerifyPKCS1v15(certificate.PublicKey.(*rsa.PublicKey), crypto.SHA256, hash[:], signature)
-	if err != nil {
-		return nil, fmt.Errorf("signature verification failed: %v", err)
+	switch pubKey := certificate.PublicKey.(type) {
+	case *ecdsa.PublicKey: // 使用 ECDSA 验证签名
+		var r, s big.Int
+		r.SetBytes(signature[:len(signature)/2])
+		s.SetBytes(signature[len(signature)/2:])
+		if ecdsa.Verify(pubKey, hash[:], &r, &s) {
+			return &payload, nil
+		} else if ecdsa.VerifyASN1(pubKey, hash[:], signature) {
+			return &payload, nil
+		}
+		return nil, fmt.Errorf("signatureBytes verification failed")
+	case *rsa.PublicKey: // 使用 RSA 验证签名
+		if err = rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, hash[:], signature); err != nil {
+			return nil, fmt.Errorf("signature verification failed: %v", err)
+		}
+	default:
+		return nil, fmt.Errorf("unsupported public key type: %T", pubKey)
 	}
 
 	return &payload, nil
